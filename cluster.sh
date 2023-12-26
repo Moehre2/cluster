bc1d3aa1f84db17881424a2996c4b49ed5c9302e
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2023-12-26 18:07:28 +0100

    Extended cluster script

--- cluster.sh
+++ cluster.sh
@@ -13,12 +13,67 @@ mkdir -p $HOME/.config
 if [ ! -f "$HOME/.config/cluster" ]; then
 	echo "SERVERS=\"\"" >> "$HOME/.config/cluster"
 	echo "#USER=" >> "$HOME/.config/cluster"
+	echo "COPYSERVERS=\"\"" >> "$HOME/.config/cluster"
 fi
 source "$HOME/.config/cluster"
-if [ "$SERVERS" = "" ]; then
-	echo "No servers available..."
-	echo "Please modify $HOME/.config/cluster and specify login nodes"
-	exit 0
+
+action="login"
+verbose=0
+source_file=""
+target_file=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -v | --verbose)
+            verbose=1
+            ;;
+        *)
+            case $1 in
+            	login)
+            		action="login"
+            		;;
+            	put)
+            		action="put"
+            		shift
+            		source_file="$1"
+            		shift
+            		target_file="$1"
+            		;;
+            	get)
+            		action="get"
+            		shift
+            		source_file="$1"
+            		shift
+            		target_file="$1"
+            		;;
+            	*)
+            		echo "Error: Unknown action"
+            		exit 1
+            		;;
+            esac
+            ;;
+    esac
+    shift
+done
+
+SERV=""
+
+if [ "$action" = "login" ]; then
+	if [ "$SERVERS" = "" ]; then
+		echo "No servers available..."
+		echo "Please modify $HOME/.config/cluster and specify login nodes"
+		exit 0
+	else
+		SERV="$SERVERS"
+	fi
+elif [[ "$action" = "put" || "$action" = "get" ]]; then
+	if [ "$COPYSERVERS" = "" ]; then
+		echo "No servers available..."
+		echo "Please modify $HOME/.config/cluster and specify copy nodes"
+		exit 0
+	else
+		SERV="$COPYSERVERS"
+	fi
 fi
 
 MIN=1; until (( (MIN<<=1) < 0 )) ;do :;done
@@ -32,10 +87,13 @@ echo -n "Password: "
 read -s password
 echo
 
-for s in $SERVERS; do
+for s in $SERV; do
 	cur_user=$(sshpass -p "$password" ssh $USER@$s "users | tr ' ' '\n' | wc -l")
 	if [[ $cur_user =~ $re_num ]]; then
 		avail=0
+		if [ "$verbose" = "1" ]; then
+			echo "$s ($cur_user)"
+		fi
 		if [ $cur_user -lt $opt_user ]; then
 			opt_user=$cur_user
 			opt=$s
@@ -48,5 +106,11 @@ if [ "$avail" = "1" ]; then
 	exit 3
 fi
 
-sshpass -p "$password" ssh $USER@$opt
+if [ "$action" = "login" ]; then
+	sshpass -p "$password" ssh $USER@$opt
+elif [ "$action" = "put" ]; then
+	sshpass -p "$password" scp "$source_file" "${USER}@${opt}:${target_file}"
+elif [ "$action" = "get" ]; then
+	sshpass -p "$password" scp "${USER}@${opt}:${source_file}" "$target_file"
+fi
 
